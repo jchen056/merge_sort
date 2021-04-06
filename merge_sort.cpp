2a47aa799e3ad0b6c0502f9c56d829d381fc2f74
Jiale (Jerry) Chen <75757467+jchen056@users.noreply.github.com>
2021-04-05 22:09:30 -0400

    Add files via upload

---
+++ merge_sort.cpp
@@ -0,0 +1,74 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+#include<vector>
+using std::vector;
+
+void merge(const vector<int>& A,const vector<int>& B, vector<int>& C){
+    for (size_t i=0;i<A.size();i++){
+        C.push_back(A[i]);
+    }
+    for (size_t j=0;j<B.size();j++){
+        C.push_back(B[j]);
+    }
+}
+
+void mergesort(const vector<int>& A,const vector<int>& B, vector<int>& C){
+    int a=A[0];
+    int b=B[0];
+    int i=0,j=0;//i is index for element in A;j is the index for element in B
+    if (a<b){
+        C.push_back(a);
+        i++;}
+    else if(a>b){
+        C.push_back(b);
+        j++;}
+    else{
+        C.push_back(a);
+        i++;
+        C.push_back(b);
+        j++;
+    }
+    
+    while (i<A.size() && j<B.size()){
+        if (A[i]<B[j]){
+            C.push_back(A[i]);
+            i++;}
+        else if (A[i]>B[j]){
+            C.push_back(B[j]);
+            j++;
+        }
+        else{
+            C.push_back(A[i]);
+            i++;
+            C.push_back(B[j]);
+            j++;
+        }
+       
+        }
+    while(i<A.size()){
+            C.push_back(A[i]);
+            i++;
+        }
+    while(j<B.size()){
+        C.push_back(B[j]);
+        j++;
+    }
+}
+int main(){
+    vector<int> A={2,4,5,9,13,17};
+    vector<int> B={1,3,7,8,18};
+    vector<int> C;
+    mergesort(A,B,C);
+    for (size_t a=0;a<C.size();a++){
+        cout<<C[a]<<" ";
+    }
+    return 0;
+}
