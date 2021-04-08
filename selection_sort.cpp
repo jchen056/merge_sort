3f6c77ae5d37c2b4682ba791b5e369bebda4d6d7
Jiale (Jerry) Chen <75757467+jchen056@users.noreply.github.com>
2021-04-07 20:46:22 -0400

    Add files via upload

---
+++ selection_sort.cpp
@@ -0,0 +1,37 @@
+
+
+#include <iostream>
+using namespace std;
+#include<vector>
+using std::vector;
+
+int index_smallest(const vector<int> &V,int start){
+    int iMin=start;//assume the first number is the smallest 
+    for(int i=start+1;i<V.size();i++){
+        if (V[i]<V[iMin])
+            iMin=i;
+    }
+    return iMin;
+}
+
+void swap(vector<int> &V,int i, int j){
+    int temp=V[i];
+    V[i]=V[j];
+    V[j]=V[i];
+}
+
+
+int main()
+{   vector<int> V;
+    int a;//store the input temporarily
+    while(cin>>a){
+        V.push_back(a);
+    }
+    
+    for (int i=0;i<V.size()-1;i++){
+        swap(V[i],V[index_smallest(V,i)]);
+    }
+    for (int j=0;j<V.size();j++)
+        cout<<V[j]<<" ";
+    return 0;
+}
